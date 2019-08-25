ed38742b525c0f83b3c15b33224c22a2bb03f2d8
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-25 05:29:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,22 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song_object = Song.new
+    song_object.save
+    song_object
+  end
+
+  def self.new_by_name(name)
+    song_object = Song.new
+    song_object.name = name
+    song_object
+  end
+
+  def self.create_by_name(name)
+    song_object = Song.new
+    song_object.name = name
+    song_object.save
+  end
+
 end
