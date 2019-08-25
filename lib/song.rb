245ca31e3539631c5cb58775b925250ecf023ee8
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-25 06:53:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,8 +50,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    self.find_by_name
-    self.create_by_name
+    if song==@@all.name
+      self.find_by_name
+    else
+      self.create_by_name
+    end 
   end
 
 end
